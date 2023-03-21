ff6de7e4e8d0bf0b394a350276d0c277476bf3f8
alihaider678 <114726530+alihaider678@users.noreply.github.com>
2023-03-21 23:04:47 +0500

    SQL queries

---
+++ SQL for Data Analysis (sample dataset + 50 queries)/queries1.sql
@@ -0,0 +1,8 @@
+-- Select everything from sales table
+
+select * from sales;
+
+-- Show just a few columns from sales table
+
+select SaleDate, Amount, Customers from sales;
+select Amount, Customers, GeoID from sales;
